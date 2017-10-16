15ba6c66f7366b395f01ceda541cb64abe8a24d6
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-16 19:51:40 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,5 @@
 def key_for_min_value(name_hash)
-  
+
   counter = 1
   items = []
   min = nil
@@ -16,27 +16,27 @@ def key_for_min_value(name_hash)
 
     if counter > name_hash.length-1
       break
-      
+
     else
       next_item = name_hash[items[counter]]
-      
+
       if counter > 1
-          
+
         if name_hash[min] >= next_item
           min = items[counter]
         end
-        
+
       else
-        
+
         if value <= next_item
           min = key
         else
           min = items[counter]
         end
-          
+
       end
-        
-    end 
+
+    end
     counter += 1
   end
   return min
